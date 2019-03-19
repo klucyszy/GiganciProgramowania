6f13f9902c6821bb9627c3bec6b40444ea852473
Kamil Łucyszyn <klucyszyn1995@gmail.com>
2019-03-20 00:24:57 +0100

    Wygaszacz v1 created

---
+++ Samples/Samples.Wygaszacz/Wygaszacz.Designer.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Forms;
+
+namespace Samples.Wygaszacz
+{
+    partial class Wygaszacz
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Timer licznik;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Initialize the Wygaszacz component
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.licznik = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // licznik
+            // 
+            this.licznik.Interval = 2000;
+            this.licznik.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Wygaszacz
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Wygaszacz";
+            this.Text = "Wygaszacz";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Wygaszacz_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Wygaszacz_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Wygaszacz_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Wygaszacz_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Samples/Samples.Wygaszacz/Wygaszacz.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Samples.Wygaszacz
+{
+    public partial class Wygaszacz : Form
+    {
+        public int X { get; private set; } // pozycja X kursora myszki
+        public int Y { get; private set; } // pozycja Y kursora myszki
+        public string[] Pictures { get; private set; } // wszystkie załadowane obrazki
+        public int CurrentPicture { get; private set; } // numer aktualnego wygaszacza ekranu
+
+        private const string picturesFolder = @"D:\Obrazy\Wygaszacz\";
+
+        public Wygaszacz()
+        {
+            InitializeComponent();
+        }
+
+        private void Wygaszacz_Load(object sender, EventArgs e)
+        {
+            SetMousePosition(); // ustaw aktualna pozycję kursora myszy
+            LoadPicturesFromFolder(picturesFolder); // załaduj zdjęcia z folderu
+            SetBackgroundImage(0); // ustawiamy pierwszy obrazek jako tło
+
+            Cursor.Hide();
+            this.WindowState = FormWindowState.Maximized;
+            licznik.Start();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            var lastPicture = Pictures.Length - 1;
+            if(CurrentPicture == lastPicture)
+            {
+                SetBackgroundImage(0);
+            }
+            else
+            {
+                SetBackgroundImage(CurrentPicture + 1);
+            }
+        }
+
+        private void Wygaszacz_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Wygaszacz_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Wygaszacz_MouseMove(object sender, MouseEventArgs e)
+        {
+            var x = Cursor.Position.X;
+            var y = Cursor.Position.Y;
+
+            if (x != X || y != Y)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void SetMousePosition()
+        {
+            X = Cursor.Position.X;
+            Y = Cursor.Position.Y;
+        }
+
+        private void LoadPicturesFromFolder(string folderPath)
+        {
+            try
+            {
+                Pictures = Directory.GetFiles(folderPath);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    $"Błąd w trakcie pobierania obrazków z folderu {picturesFolder}.", 
+                    "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if(Pictures.Length == 0)
+            {
+                MessageBox.Show(
+                    $"Folder {picturesFolder} jest pusty.",
+                    "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SetBackgroundImage(int position)
+        {            
+            this.BackgroundImage = Image.FromFile(Pictures[position]);
+            CurrentPicture = position;
+        }
+    }
+}
