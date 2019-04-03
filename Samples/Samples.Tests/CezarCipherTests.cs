b7ffb3102b4beb39aa027b0c4059a3d20acab47f
Kamil Łucyszyn <klucyszyn1995@gmail.com>
2019-04-03 22:01:58 +0200

    Test project added

---
+++ Samples/Samples.Tests/CezarCipherTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Samples.Tests
+{
+    [TestClass]
+    public class CezarCipherTests
+    {
+        [TestMethod]
+        public void extended_cezar_cipher_work_on_A_to_z()
+        {
+            //Given
+            var testValue = "A";
+            var delay = 51;
+            //When
+            var result = CezarCipher.CezarCipher.Extended(testValue.ToString(), delay);
+            //Then           
+            Assert.AreEqual("z", result);
+        }
+
+        [TestMethod]
+        public void extended_cezar_cipher_work_on_B_to_A()
+        {
+            //Given
+            var testValue = "B";
+            var delay = 51;
+            //When
+            var result = CezarCipher.CezarCipher.Extended(testValue.ToString(), delay);
+            //Then           
+            Assert.AreEqual("A", result);
+        }
+
+        [TestMethod]
+        public void extended_cezar_cipher_work_on_A_to_a()
+        {
+            //Given
+            var testValue = "A";
+            var delay = 26;
+            //When
+            var result = CezarCipher.CezarCipher.Extended(testValue.ToString(), delay);
+            //Then           
+            Assert.AreEqual("a", result);
+        }
+
+        [TestMethod]
+        public void extended_cezar_cipher_work_on_Z_to_z()
+        {
+            //Given
+            var testValue = "Z";
+            var delay = 26;
+            //When
+            var result = CezarCipher.CezarCipher.Extended(testValue.ToString(), delay);
+            //Then           
+            Assert.AreEqual("z", result);
+        }
+    }
+}
---
+++ Samples/Samples.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Samples.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Samples.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("078e8660-c88b-4fc8-9fa2-637281c0fd6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
