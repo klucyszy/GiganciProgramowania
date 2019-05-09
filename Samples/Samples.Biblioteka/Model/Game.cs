beb12a1c7471d5d667897946528f41b92b29edfb
Kamil Łucyszyn <klucyszyn1995@gmail.com>
2019-05-09 23:25:36 +0200

    Game class created

---
+++ Samples/Samples.Biblioteka/Model/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Biblioteka.Model
+{
+    public class Game : LibraryObject
+    {
+        public int MinimumAge { get; set; }
+        public string  Title { get; set; }
+
+        public Game(decimal price, string desc, int minAge, string title) 
+            : base(price, desc)
+        {
+            MinimumAge = minAge;
+            Title = title;
+        }
+    }
+}
