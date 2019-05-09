27a3cbcf386dd28dea32beb1baa359c8e10d0c2f
Kamil Łucyszyn <klucyszyn1995@gmail.com>
2019-05-09 23:19:06 +0200

    Added Model folder with LibraryObject class

---
+++ Samples/Samples.Biblioteka/Model/LibraryObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Biblioteka.Model
+{
+    public class LibraryObject
+    {
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
