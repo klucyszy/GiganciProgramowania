efe2bf1d2e2b53479a040237f28cdd8e76192d80
Kamil Łucyszyn <klucyszyn1995@gmail.com>
2019-05-09 23:44:45 +0200

    AddGame added to Library class, fix: Games list setter make private

---
+++ Samples/Samples.Biblioteka/Model/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Biblioteka.Model
+{
+    public class Library
+    {
+        public List<Game> Games { get; private set; }
+
+        public Library()
+        {
+            Games = new List<Game>();
+        }
+
+        public void AddGame(Game game)
+        {
+            if (!Games.Contains(game))
+            {
+                Games.Add(game);
+            }
+        }
+    }
+}
